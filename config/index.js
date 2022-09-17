c8c25cf96c718e52b2127fe7e348d36b32de1235
Crystal-zhi <924518270@qq.com>
2022-09-17 15:05:29 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "okres5_kEkzWnqI2Eq8BX6H8iEL0",
+      id: "okres55rckvnDKoey6s4CVL-hdh8",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "bvLfLI1G0TfL2zqtMUXuyF0aPbgrQEx8l7KnmHCcf-A",
       // 所在省份
