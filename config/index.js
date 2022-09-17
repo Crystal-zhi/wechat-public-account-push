ce1c32cf410374a4ed92698e6cd31c2b6b1bb008
Crystal-zhi <924518270@qq.com>
2022-09-17 14:57:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "okres5_kEkzWnqI2Eq8BX6H8iEL0",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "2LdT9MSTq8oViT5PYYygl8tmRfP0SWIBWReWoF5VnwA",
+      useTemplateId: "wnGbzjFltG5N18-9wtq1ZbTn-KQYbKcrKpTy6vEe5jA",
       // 所在省份
       province: "贵州",
       // 所在城市
